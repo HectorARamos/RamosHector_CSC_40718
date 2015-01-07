49426c755f259278b7027dc9f3e34ae6850f3795
hr2531311 <HectorARamosEDU@gmail.com>
2015-01-06 21:23:13 -0800

    Signed-off-by: hr2531311 <HectorARamosEDU@gmail.com>

---
+++ Homework/Asignment/ComputersMakeMistakes/main.cpp
@@ -0,0 +1,35 @@
+/* 
+   File:   main.cpp
+   Author: Hector A. Ramos
+   Created on January 6, 2015, 9:00 PM
+   Purpose: To prove how not knowing how to code well can produce mistakes
+ */
+
+//System Libraries
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here!
+
+
+int main(int argc, char** argv) {
+    //Declare Variables
+    short a,b,c;
+    //Promt User for a and b
+    cout<<"Input 2 floatvariable Values"<<endl;
+    cin>>a>>b;
+    //Calculate the sum
+    c=a+b;
+    //Output the results
+    cout<<"The sum of c=a+b ->"<<c<<endl;
+    //Exit stage right!
+    return 0;
+}
+
