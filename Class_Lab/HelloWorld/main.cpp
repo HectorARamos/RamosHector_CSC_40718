dd70fbb66f26a233a58605d9de85ddec25aa84c9
hr2531311 <HectorARamosEDU@gmail.com>
2015-01-06 20:03:02 -0800

    Signed-off-by: hr2531311 <HectorARamosEDU@gmail.com>

---
+++ Class_Lab/HelloWorld/main.cpp
@@ -0,0 +1,27 @@
+/* 
+   File:   main.cpp
+   Author: Hector A. Ramos
+   Created on January 6, 2015, 6:05 PM
+   Purpose: Our first program
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here!
+
+
+int main(int argc, char** argv) {
+    //Output Hello World
+    cout<<"Hello World"<<endl;
+    //Exit stage right!
+    return 0;
+}
+
