a8bd3b57274dbda4eab7e0621ff5d12318956e31
HectorARamos <HectorARamosEDU@gmail.com>
2015-01-08 20:27:51 -0800

    Signed-off-by: HectorARamos <Paradise14>

--- Homework/Assignment 1/Gaddis_7thEd_Chap2_Prob11/main.cpp
+++ Homework/Assignment 1/Gaddis_7thEd_Chap2_Prob11/main.cpp
@@ -1,19 +1,37 @@
 /* 
- * File:   main.cpp
- * Author: rcc
- *
- * Created on January 8, 2015, 8:12 PM
+   File:   main.cpp
+   Author: Hector A. Ramos
+   Created on January 8, 2015, 8:15 PM
+   Purpose: Homework Assignment (Gaddis, 7th Edition, Chapter 2, Problem 11)
  */
 
-#include <cstdlib>
-
+//System Libraries
+#include <iostream>
 using namespace std;
 
-/*
- * 
- */
-int main(int argc, char** argv) {
+//User Libraries
 
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here!
+
+int main(int argc, char** argv) {
+  //Declare and Initialize Variables
+    float gasTank=20;//max gallons in our gas tank
+    float inTown=21.5;//In town gas miles per gallon
+    float onHway=26.8;//Highway miles per gallon
+    //Calculate the total miles in an out of town
+    float disTown=gasTank*inTown;
+    float disHway=gasTank*onHway;
+    //Output the results
+    cout<<"Distance in Town on a tank of gas    = ";
+    cout<<disTown<<"(miles)"<<endl;
+    cout<<"Distance on Highway on a tank of gas = ";
+    cout<<disHway<<"(miles)"<<endl;
+
+    //Exit stage right!
     return 0;
 }
 
